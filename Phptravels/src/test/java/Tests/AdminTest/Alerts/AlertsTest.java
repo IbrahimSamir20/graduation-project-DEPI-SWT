ec6e32fabbd75d99e05a585f5e1c605eb030b8c0
omartalaat-xmero <136380940+omartalaat-xmero@users.noreply.github.com>
2025-04-19 15:52:40 +0200

    omar

--- Phptravels/src/test/java/Tests/AdminTest/Alerts/AlertsTest.java
+++ Phptravels/src/test/java/Tests/AdminTest/Alerts/AlertsTest.java
@@ -1,25 +1,33 @@
 package Tests.AdminTest.Alerts;
+import Helper.AdminLoginHelper;
 import PhptravelsPages.AdminPage.Alerts.AlertsBase;
 import Tests.TestBase.AdminTestBase;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 public class AlertsTest extends AdminTestBase {
     AlertsBase alert;
+    AdminLoginHelper login;
     @BeforeMethod
-    public void init(){
+    public void init() throws InterruptedException {
+        login = new AdminLoginHelper();
+        Thread.sleep(2000);
         alert = new AlertsBase(AdminDriver);
+        Thread.sleep(2000);
     }
     @Test
     public void VerifyFunctionalityOfAlertButton () throws InterruptedException{
+        login.adminLogin();
+        Thread.sleep(2000);
         alert.displayAlerts();
         Thread.sleep(2000);
     }
     @Test
     public void VerifyFunctionalityOfDeleteAlertButton () throws InterruptedException{
+        login.adminLogin();
+        Thread.sleep(5000);
         alert.displayAlerts();
         Thread.sleep(2000);
-        alert.DeleteAlert();
+        alert.deleteAlert();
         Thread.sleep(2000);
     }
-
 }
