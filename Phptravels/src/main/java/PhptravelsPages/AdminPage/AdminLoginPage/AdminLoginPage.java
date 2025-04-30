92164d4af67311ff75b1f9705bb848921b94de2e
Ibrahim Samir <ibrahimsamir.tec@gmail.com>
2025-04-30 16:15:28 +0300

    Save local changes before pull

--- Phptravels/src/main/java/PhptravelsPages/AdminPage/AdminLoginPage/AdminLoginPage.java
+++ Phptravels/src/main/java/PhptravelsPages/AdminPage/AdminLoginPage/AdminLoginPage.java
@@ -6,9 +6,14 @@ public class AdminLoginPage extends PageBase {
     By Email=By.xpath("(//*[@class=\"form-control\"])[1]");
     By Password=By.xpath("(//*[@class=\"form-control\"])[2]");
     By LoginButton=By.xpath("//*[@class=\"login_button mt-3 btn btn-primary w-100 btn-lg\"]");
+    By Email2 = By.xpath("//input[@id=\"email\"] ");
+
     public AdminLoginPage(WebDriver driver) {
         super(driver);
     }
+
+
+
     public void fillEmail(String email){
         sendKeys(Email,email);
     }
