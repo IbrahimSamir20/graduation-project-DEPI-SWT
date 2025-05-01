3aa033c5c7fb4a77fa7ed39b09ccee0686d7ff62
omartalaat-xmero <136380940+omartalaat-xmero@users.noreply.github.com>
2025-05-02 00:08:21 +0300

    omar

--- Phptravels/src/main/java/PhptravelsPages/PageBase/PageBase.java
+++ Phptravels/src/main/java/PhptravelsPages/PageBase/PageBase.java
@@ -1,24 +1,20 @@
 package PhptravelsPages.PageBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 public class PageBase {
     public WebDriver driver;
-    public static Duration Wait= Duration.ofSeconds(200);
+    public static long Wait =200;
     public PageBase(WebDriver driver) {
         this.driver = driver;
     }
-    public void waiTimeToBeClicable(By element){
-        WebDriverWait wait;
-        wait = new WebDriverWait(driver, Wait);
+    public void waiTimeToBeClickable(By element){
+        WebDriverWait wait = new WebDriverWait(driver,Wait);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
     public void waiTimeToBeVisible(By element){
@@ -32,14 +28,13 @@ public class PageBase {
         driver.findElement(element).sendKeys(value);
     }
     public void clickOnElement (By element){
-        waiTimeToBeClicable(element);
+        waiTimeToBeClickable(element);
         driver.findElement(element).click();
     }
     public void selectDropDown (By element, String option){
-        Select oprions = new Select(driver.findElement(element));
-        oprions.selectByVisibleText(option);
+        Select options = new Select(driver.findElement(element));
+        options.selectByVisibleText(option);
     }
-/*******************************************/
     public String getText(By element){
         waiTimeToBeVisible(element);
         return driver.findElement(element).getText();
@@ -58,10 +53,8 @@ public class PageBase {
         Actions actions = new Actions(driver);
         actions.dragAndDrop(source, target).perform();
     }
-
-    /**************************************************/
     public void sendKeysToDropList (By element1,By element2, String keys) {
-        waiTimeToBeClicable(element1);
+        waiTimeToBeClickable(element1);
         driver.findElement(element1).click();
         waiTimeToBeVisible(element2);
         driver.findElement(element2).sendKeys(keys + Keys.ENTER);
